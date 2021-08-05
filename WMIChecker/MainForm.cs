90e29235e6eba3a00e80b0c17b59c39d903bd123
hikaru <alclimb@users.noreply.github.com>
2021-08-05 14:15:20 +0900

    feat: System.Management パッケージを追加

--- WMIChecker/MainForm.cs
+++ WMIChecker/MainForm.cs
@@ -1,21 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WMIChecker
+﻿namespace WMIChecker
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     public partial class MainForm : Form
     {
         public MainForm()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            // WMI取得処理
+            Utils.GetWmiObj("Win32_ComputerSystemProduct", (obj) =>
+            {
+                System.Diagnostics.Debug.WriteLine(obj.Properties["Name"].Value.ToString());
+            });
         }
 
+    }
+
+    public static class Utils
+    {
+        public static void GetWmiObj(string classPath, Action<System.Management.ManagementBaseObject> action)
+        {
+            // WMI取得処理
+            using (var objClass = new System.Management.ManagementClass(classPath))
+            using (var objCollection = objClass.GetInstances())
+            {
+                foreach (var obj in objCollection)
+                {
+                    action(obj);
+
+                    obj.Dispose();
+                }
+            }
+        }
     }
 }

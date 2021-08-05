ab70bc7911814628aec09f44140126da4364e6ce
hikaru <alclimb@users.noreply.github.com>
2021-08-05 14:02:28 +0900

    refactor: フォームの名称を変更

--- WMIChecker/MainForm.Designer.cs
+++ WMIChecker/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WMIChecker
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,10 +29,17 @@ namespace WMIChecker
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "WMI Checker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
